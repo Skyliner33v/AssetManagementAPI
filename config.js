3c9a2fc6173b425d0037e3aa102bd9ca402e83dd
parentj <ParentJ@WSDOT.WA.GOV>
2019-01-30 08:49:25 -0800

    Create config file to connect to the database

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+    development: {
+        driver: 'msnodesqlv8',
+        connectionString: 'Driver=SQL Server Native Client 11.0;Server=SQLEXPRESS01;Database=AssetManagement;Trusted_Connection=yes'
+    }
+};
+module.exports = config;
