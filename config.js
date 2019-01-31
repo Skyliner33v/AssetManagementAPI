a26cfb4dfc58dad416893a2f41c0984843049e4c
parentj <ParentJ@WSDOT.WA.GOV>
2019-01-31 11:07:02 -0800

    Add new connections to 02T and 02P servers

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 var config = {
-    development: {
+    testLocalAssetManagement: {
         driver: 'msnodesqlv8',
         connectionString: 'Driver={SQL Server Native Client 11.0};Server={localhost\\SQLEXPRESS01};Database={LocalAssetManagement};Trusted_Connection={yes};'
+    },
+    test02tAssetManagement: {
+        driver: 'msnodesqlv8',
+        connectionString: 'Driver={SQL Server Native Client 11.0};Server={HQOLYMBRGSL202T};Database={AssetManagement};Trusted_Connection={yes};'
+    },
+    prod02pBridgeData: {
+        driver: 'msnodesqlv8',
+        connectionString: 'Driver={SQL Server Native Client 11.0};Server={HQOLYMBRGSL202P};Database={BridgeData};Trusted_Connection={yes};'
     }
 };
 module.exports = config;
