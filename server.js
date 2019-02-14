1d34a2c2331d9a0f38fb64bb85a924327db05a23
Parent <ParentJ@WSDOT.WA.GOV>
2019-02-14 14:28:15 -0800

    Update routes and remove unecessary ones

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express'); // minimalist web framework for Node.js
 const app = express();
-const sql = require('mssql/msnodesqlv8') //mssql with MS driver for SQL Server
-// added windows authentication
+const sql = require('mssql/msnodesqlv8') //mssql with MS driver for SQL Server / added windows authentication
 
-var env = process.env.NODE_ENV || 'testLocalAssetManagement';
+var env = process.env.NODE_ENV || 'test02tAssetManagement';
 var sqlConfig = require('./config')[env];
 
 // Start server and listen on http://localhost:8081/
@@ -20,18 +19,19 @@ const connection = new sql.ConnectionPool(sqlConfig, function (err) {
     }
 })
 
-// Define a simple route
+// Define the root route
 app.get('/', function (req, res) {
     res.json({
-        "message": "Welcome to WSDOT AssetManagement Sample App."
+        "message": "Welcome to WSDOT Asset Management API."
     });
 });
+
 
 // Define a route to the Bridge Table
 app.get('/api/bridges', function (req, res) {
     connection.connect().then(pool => { //Using a single connection pool is recommended
         var conn = pool.request()
-        var string = 'SELECT * FROM Bridge'
+        var string = 'SELECT * FROM BRIDGE'
         return conn.query(string)
     }).then(result => {
         let rows = result.recordset
@@ -47,11 +47,12 @@ app.get('/api/bridges', function (req, res) {
     });
 });
 
-// Define a route to the Element Definitions Table
-app.get('/api/elementDefinitions', function (req, res) {
+
+// Define a route to the Roadway Table
+app.get('/api/roadway', function (req, res) {
     connection.connect().then(pool => { //Using a single connection pool is recommended
         var conn = pool.request()
-        var string = 'SELECT * FROM PON_ELEM_DEFS'
+        var string = 'SELECT * FROM ROADWAY'
         return conn.query(string)
     }).then(result => {
         let rows = result.recordset
@@ -67,7 +68,8 @@ app.get('/api/elementDefinitions', function (req, res) {
     });
 });
 
-// Define a route to the Element Definitions Table
+
+// Define a route to the Inspections Table
 app.get('/api/inspections', function (req, res) {
     connection.connect().then(pool => { //Using a single connection pool is recommended
         var conn = pool.request()
@@ -87,6 +89,29 @@ app.get('/api/inspections', function (req, res) {
     });
 });
 
+
+// Define a route to the Element Data Table
+app.get('/api/elementData', function (req, res) {
+    connection.connect().then(pool => { //Using a single connection pool is recommended
+        var conn = pool.request()
+        var string = 'SELECT * FROM PON_ELEM_INSP'
+        return conn.query(string)
+    }).then(result => {
+        let rows = result.recordset
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.status(200).json(rows);
+        connection.close();
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({
+            message: err
+        })
+        connection.close();
+    });
+});
+
+
+/* Hold for future use
 // Define a route to the BridgeData/tblMaster Table
 app.get('/api/tblMaster', function (req, res) {
     connection.connect().then(pool => { //Using a single connection pool is recommended
@@ -105,4 +130,4 @@ app.get('/api/tblMaster', function (req, res) {
         })
         connection.close();
     });
-});
+}); */
